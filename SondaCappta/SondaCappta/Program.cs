0492fde24dfb2a6bb8740801a96b37ac0ad56b4c
lfmundim <lucasfmundim@ufmg.br>
2019-09-22 16:36:37 -0300

    repo startup

---
+++ SondaCappta/SondaCappta/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SondaCappta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
